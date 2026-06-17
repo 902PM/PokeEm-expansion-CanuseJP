ea4d8df773e5990cde45b1d4d83e817cfebef1a5
goukei13 <Ehero.devil@gmail.com>
2026-06-18 07:11:04 +0900

    エがまだ変だしテキスト上書き治ってない

--- src/text.c
+++ src/text.c
@@ -76,8 +76,8 @@ static const u8 sDownArrowYCoords[] = { 0, 1, 2, 1 };
 
 static bool32 IsJapaneseGlyph(u16 glyphId)
 {
-    return glyphId != CHAR_SPACE
-        && glyphId <= JAPANESE_CHAR_END;
+    return (glyphId >= 0x01 && glyphId <= 0x53)
+        || (glyphId >= 0x55 && glyphId <= 0xA0);
 }
 
 static const struct GlyphWidthFunc sGlyphWidthFuncs[] =
